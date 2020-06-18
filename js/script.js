f24aef90a45d6800bdca06837818304950ae714f
cheonggil.bang <fqj102@gmail.com>
2020-06-19 07:20:55 +0900

    add content

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 $(function () {
     var $loginForm = $('#loginForm');
     var $joinForm = $('#joinForm');
+    var $userWrap = $('#userWrap');
+    var $contentWrap = $("#contentWrap");
 
     $("#loginBtn").click(function(e){
         e.preventDefault();
@@ -12,5 +14,12 @@ $(function () {
        e.preventDefault();
        $joinForm.show();
        $loginForm.hide();
+    });
+
+    $("#logoutBtn").click(function (e) {
+        e.preventDefault();
+        alert("로그아웃되었습니다.");
+        $userWrap.show();
+        $contentWrap.hide();
     });
 })
